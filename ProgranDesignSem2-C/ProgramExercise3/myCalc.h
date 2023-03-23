fd37a10fed4de9e2aee00050b3178f4fff596a2b
jeremy1031cute <113334989+jeremy1031cute@users.noreply.github.com>
2023-03-23 10:34:56 +0800

    define

--- ProgranDesignSem2-C/ProgramExercise3/myCalc.h
+++ ProgranDesignSem2-C/ProgramExercise3/myCalc.h
@@ -2,3 +2,9 @@ int precedCmp(char *oper1, char *oper2);
 int infixCalc(char **tokens, int tokenLens);
 int prefixCalc(char **tokens, int tokenLens);
 int postfixCalc(char **tokens, int tokenLens);
+
+//precedCmp
+#define isAddOrMinus(x) (x == '+' || x == '-') ? 1 : 0
+
+//infixCalc
+//#include <ctype.h>//isdigit();
--- ProgranDesignSem2-C/ProgramExercise3/myIO.h
+++ ProgranDesignSem2-C/ProgramExercise3/myIO.h
@@ -4,3 +4,15 @@ void _free_tokens(char **tokens, int tokenLens);
 void _exp_print(char **expression, int expLens, int mode);
 void _calcResult_print(int result);
 void _error_message(int condition);
+//for different condition(just like choose)
+#define TooMany 1
+#define TooLess 2
+#define Unknown 3
+#define Duplicate 4
+
+#define INFIX 0
+#define PREFIX 1
+#define POSTFIX 2
+
+#define Rev 1
+#define Nor 2
