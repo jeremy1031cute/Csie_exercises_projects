807ee19991edb10e58a8c60209b3a5cb7a08ce39
jeremy1031cute <113334989+jeremy1031cute@users.noreply.github.com>
2023-03-24 17:19:26 +0800

    trying

--- ProgramDesignSem2/Exercises/ProgramExercise4.c
+++ ProgramDesignSem2/Exercises/ProgramExercise4.c
@@ -1,31 +1,66 @@
 #include <stdio.h>
 int main{
     int option;
+    /*option 1*/
+    int num = 0;
 
-    printf("Enter a command: ");
-    scanf("%d", &option);
+    /*option 2*/
+    int detail_num = 0;
 
+    /*option 3*/
+    int check;
 
-    for(int i = 0; i < ; i++){
+    struct history{
+        char crime_name[20];
+        int date;
+        int penalty_months;
+    };
+
+    struct data{
+        char name[15];
+        struct history detail[10];//treat "struct history" as a struct type
+        int count;
+    }criminal[10];
+
+    while(1){
+        printf("Enter a command: ");
+        scanf("%d", &option);
         switch(option){
-        case 0:
-            //return 0;
-            break;
         case 1:
             printf("Enter a criminal's name: ");
-            printf("Created new criminal data (number %d).\n");/* number starts from 0 */
+            scanf("%d", &criminal[num].name);
+            if(num <= 10)
+                printf("Created new criminal data (number %d).\n", num++);/* number starts from 0 */
+            else
+                num = 0;//i am not sure xp
             //scanf("%d", &  );
             break;
         case 2:
             printf("Enter a number of the criminal: ");
+            scanf("%d", &detail_num);
             printf("Enter crime: ");
+            fgets(criminal[detail_num].detail.crime_name, 20, stdin);
+            int len = strlen(criminal[detail_num].detail.crime_name) - 1;
+            criminal[detail_num].detail.crime_name[len] = '\0';
             printf("Enter date: ");
+                scanf("%d", &criminal[detail_num].detail.date);
             printf("Enter penalty: ");
+                scanf("%d", &criminal[detail_num].detail.penalty_months);
             break;
         case 3:
-
+            printf("Enter a number of the criminal: ");
+            scanf("%d", &check);
+            printf("Name: %s\n====================\n", criminal[check].name)
+            for(int i; i < ; i++){
+                printf("Year: %d, Crime: %s, Penalty: %d months\n", , , );
+            }
+            printf("====================\n");
+            break;
+        default:
             break;
         }
+
+        if(option == 0) break;
     }
 
 
--- ProgramDesignSem2/testt.c
+++ ProgramDesignSem2/testt.c
@@ -1,6 +1,24 @@
+// Online C compiler to run C program online
 #include <stdio.h>
+#include<string.h>
 int main(){
-printf("omg;");
-printf("yubi  ");
-return 0;
+
+    char ff[20];
+    fgets(ff, 20, stdin);
+    int len = strlen(ff);
+    printf("len:%d\n", len);
+    for(int i = 0; i<20; i++){
+        if(ff[i] == '\0')//the last one is defenitely,must be '\0'
+            printf("0");
+        else if(ff[i] == '\n')
+            printf("n");
+        else
+            printf("x");
+    }
+    printf("\nlen:%d\n", len);
+    printf("%s", ff);
+    //you type 18 char and u will get 19 len(18+'\n')
+    //you type 19 char and u will get 19 len(19+'\0')
+    //we could know fgets() won't add '\n' when user scanf more than(include) n-1 char
+        return 0;
 }
