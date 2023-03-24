ac48630fcde2c695e3d93aae9e6c97297bbddc44
jeremy1031cute <113334989+jeremy1031cute@users.noreply.github.com>
2023-03-24 18:22:49 +0800

    trying

--- ProgramDesignSem2/Exercises/ProgramExercise4.c
+++ ProgramDesignSem2/Exercises/ProgramExercise4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main{
+#include <string.h>
+int main(){
     int option;
+    int len;
     /*option 1*/
     int num = 0;
 
@@ -18,7 +20,7 @@ int main{
 
     struct data{
         char name[15];
-        struct history detail[10];//treat "struct history" as a struct type
+        struct history detail;//treat "struct history" as a struct type
         int count;
     }criminal[10];
 
@@ -28,7 +30,10 @@ int main{
         switch(option){
         case 1:
             printf("Enter a criminal's name: ");
-            scanf("%d", &criminal[num].name);
+            fgets(criminal[num].name, 15, stdin);
+            //scanf("%s", &criminal[num].name);
+            len = strlen(criminal[num].name)-1;
+            criminal[num].name[len] = '\0';
             if(num <= 10)
                 printf("Created new criminal data (number %d).\n", num++);/* number starts from 0 */
             else
@@ -40,7 +45,7 @@ int main{
             scanf("%d", &detail_num);
             printf("Enter crime: ");
             fgets(criminal[detail_num].detail.crime_name, 20, stdin);
-            int len = strlen(criminal[detail_num].detail.crime_name) - 1;
+            len = strlen(criminal[detail_num].detail.crime_name) - 1;
             criminal[detail_num].detail.crime_name[len] = '\0';
             printf("Enter date: ");
                 scanf("%d", &criminal[detail_num].detail.date);
@@ -50,10 +55,10 @@ int main{
         case 3:
             printf("Enter a number of the criminal: ");
             scanf("%d", &check);
-            printf("Name: %s\n====================\n", criminal[check].name)
-            for(int i; i < ; i++){
-                printf("Year: %d, Crime: %s, Penalty: %d months\n", , , );
-            }
+            printf("Name: %s\n====================\n", criminal[check].name);
+            //for(int i; i < ; i++){
+                printf("Year: %d, Crime: %s, Penalty: %d months\n", criminal[check].detail.date, criminal[check].detail.crime_name, criminal[check].detail.penalty_months);
+            //}
             printf("====================\n");
             break;
         default:
