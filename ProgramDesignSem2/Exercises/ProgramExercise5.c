00ec2c4dfbb067bcca70d0e00b7b61b3ba791096
jeremy1031cute <113334989+jeremy1031cute@users.noreply.github.com>
2023-04-01 15:24:44 +0800

    Update ProgramExercise5.c

--- ProgramDesignSem2/Exercises/ProgramExercise5.c
+++ ProgramDesignSem2/Exercises/ProgramExercise5.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 size_t my_strlen(const char* str); //Returns the length of the C string str.
-//char* my_strcpy(char* dest, const char* source); //Copy string from source to dest.
+int clean_new_line_character(char* str, int l);
+char* my_strcpy(char* dest, const char* source); //Copy string from source to dest.
 //char* my_strcat(char* dest, const char* source); //Concatenate strings from source to dest.
 //int my_strcmp(const char* str1, const char* str2); //Compares str1 to str2. The rules of comparison are the same as the strcmp().
 
 int main(){
-    char *str;
-    str = (char *) malloc(sizeof(char) * 100);
-    fgets(str, 100, stdin);
+    char *str, *str_new, *str_longest;
+    str_longest = (char *) calloc(sizeof(char) * 101);
+    *str_longest = '\0';
+    int sentence_count = 0;
+    int len, longest_len = 0;
+    /*char* combine;
+    combine = (char *) malloc(sizeof(char) * 8192);*/
 
-    printf("%d", my_strlen(str));
-    free(str);
-    /*while(1){
-        fgets(str, 100, stdin);
+    //do{
+        str = (char *) malloc(sizeof(char) * 101);
+        fgets(str, 101, stdin);
+        sentence_count++;
+        len = my_strlen(str);
+        len = clean_new_line_character(str, len);
+        if(len >= longest_len){
+            longest_len = len;
+            free(str_longest);
+            my_strcpy(str_longest, str);
+        }else if(len == longest_len){
+            //my_strcmp();
+        }
 
-    }*/
-    //printf("");
+        free(str);
+    //}while(1)
+
+
+    printf("number of sentence: %d\n", sentence_count);
+    printf("longest sentence: %s\n", str_longest);
+    printf("longest length: %d\n", longest_len);
+    //printf("%s", combine);
     return 0;
 }
 
 size_t my_strlen(const char* str){
     size_t sum = 0;
-    while(*(str + sum)){
+    while(*(str + sum)){ //ASCII of '\0' is 0 (Implicit Conversions)
         sum++;
     }
-    /*if(*(str + sum) == '\n'){
-        *(str + sum) = '\0';
-        sum--;
-    }*/
     return sum;
 }
 
+int clean_new_line_character(char* str, int l){
+    if(*(str + l - 1) == '\n'){
+        *(str + l - 1) = '\0';
+        l--;
+    }
+    return l;
+}
+
+char* my_strcpy(char* dest, const char* source){
+    int index = 0;
+
+    while(*(source + index) != '\0'){
+        *(dest + index) = *(source + index);
+        index++;
+    }
+    *(dest + index) = '\0';
+    return dest;
+ }
 //Knowledge learn fron min-huang teaching assistant
 /*
     1.When you use Windos system like txt. file, it will have two new line character '\n' and '\r',the two character will be printed together,
