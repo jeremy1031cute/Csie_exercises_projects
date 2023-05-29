ac1dd97a90d7ce32ada205404b2e514b4935dd33
jeremy1031cute <113334989+jeremy1031cute@users.noreply.github.com>
2023-05-29 13:13:26 +0800

    5/29 test done
    需要增加回收機制

--- ProgramDesignSem2/Untitled1.c
+++ ProgramDesignSem2/Untitled1.c
@@ -1,64 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define NAME_LENGTH 49
+#include <string.h>
+#include <time.h>
 
 int main(){
-    int course_num;
-    scanf("%d", &course_num);
-
-    struct course{
-        int course_id;
-        struct course *next;
-        struct course *prior;
-        int next_num;
-        int prior_num;
-        char name[NAME_LENGTH];
-    };
-
-    struct course *first = NULL, *current = NULL;
-
-    for(int i = 0; i < course_num; i++){
-        struct course *new_node;
-        new_node = malloc(sizeof(struct course));
-        scanf(" %d %d %d %s", &new_node->course_id, &new_node->next_num, &new_node->prior_num, new_node->name);
-        if(i == 0){
-            first = new_node;
-            current = first;
-            continue;
+    srand( time(NULL) );
+    int record[10] = {0,0};
+    int i , j;
+    int max = 20;//int max = max_id();
+    for (i = 1; i <= 10; i++) {
+        if (max <10) {
+            printf("The number of questions is not enough!(< 10)\n");
+            return NULL;
         }
-        if(current->next_num == new_node->course_id){
-            current->next = new_node;
-        }else{
-            current->next = NULL;
-        }
-        if(new_node->prior_num == current->course_id){
-            new_node->prior = current;
-        }else{
-            current->next = NULL;
-        }
-        current = new_node;
+        do {
+            record[i - 1] = rand() % (max - 1 + 1) + 1;
+            for (j = 1; j < i; j++) {
+                if(record[i - 1] == record[j - 1]){
+                    break;
+                }
+            }
+        } while (j != i);
+        printf("%d ", record[i - 1]);
     }
-
-    current = first;
-    printf("Current Course: %s\n", current->name);
-    while(1){
-        char check;
-        printf("[n] Next course. [p] Prior course [q] Quit:");
-        scanf(" %c", &check);
-        if(check == 'n' && current->next == NULL){
-            printf("There is no next course.\n");
-        }else if(check == 'p' && current->prior == NULL){
-            printf("There is no previous course.\n");
-        }else if(check == 'q'){
-            break;
-        }else if(check == 'n'){
-            current = current->next;
-            printf("Current Course: %s\n", current->name);
-        }else if(check == 'p'){
-            current = current->prior;
-            printf("Current Course: %s\n", current->name);
-        }
-    }
+    printf("\n");
 
     return 0;
 }
