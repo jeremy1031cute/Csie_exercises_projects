215a3bbf0446d1576854edb453821f174f91c911
Jeremy <g536892100@gmail.com>
2023-12-17 16:43:25 +0800

    16 finished

--- Data_structure/ITSA_basic/16子字串出現次數.c
+++ Data_structure/ITSA_basic/16子字串出現次數.c
@@ -12,7 +12,7 @@ int main()
 
 
     for (int i = 0; i < len2-1; i++) {
-        for (int j = 0; j < len1-1; j++) { //delete '\n'
+        for (int j = 0; j < len1-1; j++) { //notice '\n'
             if (str2[i + j] != str1[j]) {
                 flag = 1;
                 break;
